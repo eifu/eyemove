191546b92915ff4a5cb7d8bfd5c6366014dc7ce4
eifu <etomita18@gmail.com>
2016-07-09 21:35:02 -0400

    row by row algorithm var 1

--- main.go
+++ main.go
@@ -48,7 +48,6 @@ func expandRGBA(img image.Image) *image.RGBA {
 }
 
 func luminosity(r, g, b uint8) float64 {
-
 	return float64(r)*0.2126 + float64(g)*0.7152 + float64(b)*0.0722
 }
 
@@ -82,7 +81,7 @@ func dx(img image.Image, x, y int) float64 {
 
 	l0 := luminosity(uint8(r0), uint8(g0), uint8(b0))
 	l2 := luminosity(uint8(r2), uint8(g2), uint8(b2))
-	//	fmt.Println(uint8(r0), uint8(g0), uint8(b0), l0)
+
 	l1 := l2 - l0
 	if l1 == 0 {
 		return 1.0
@@ -92,8 +91,6 @@ func dx(img image.Image, x, y int) float64 {
 }
 
 func gradient_magnitude(dx, dy float64) float64 {
-	//	fmt.Println(dx, dy, int(math.Sqrt(dx*dx+dy*dy)))
-
 	return math.Sqrt(dx*dx + dy*dy)
 }
 
@@ -103,7 +100,6 @@ func gradient_theta(dx, dy float64) float64 {
 	}
 
 	th := math.Atan2(dy, dx) * (180 / math.Pi)
-
 	return th
 }
 
@@ -117,7 +113,7 @@ func main() {
 	file, err := os.Open("data/test1.jpg")
 	defer file.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "main %v\n", err)
+		fmt.Fprintf(os.Stderr, "main open file :%v\n", err)
 		os.Exit(1)
 	}
 
@@ -126,34 +122,47 @@ func main() {
 		fmt.Fprintf(os.Stderr, "main read file :%v\n", err)
 		os.Exit(1)
 	}
-	img = expandRGBA(img)
 
-	//	png.Encode(os.Stdout, img)
+	img = expandRGBA(img) // settle it black(0x00) and white(0xFF)
 
 	xmax, ymax := img.Bounds().Max.X, img.Bounds().Max.Y
-	//chunk_list := make([]Chunk, (xmax-1)*(ymax-1))
-	var chunk_list []Chunk
-	var theta, magnitude float64
-	for y := 0; y < ymax; y++ {
-		for x := 0; x < xmax; x++ {
-			dx, dy := dx(img, x, y), dy(img, x, y)
-			theta = gradient_theta(dx, dy)
-			magnitude = gradient_magnitude(dx, dy)
-			chunk_list = append(chunk_list, Chunk{x: x, y: y, theta: theta, magnitude: magnitude})
-		}
-	}
+
+	var row_array = make([]int, xmax-1)
 
 	rect := img.Bounds()
 	nimg := image.NewRGBA(rect)
 
-	for _, chunk := range chunk_list {
-		//	fmt.Println(chunk.theta, chunk.magnitude)
-		if math.Abs(float64(chunk.theta)) <= 120 && chunk.magnitude >= 5 {
-			nimg.Set(chunk.x, chunk.y, color.RGBA{0x0, 0x0, 0x0, 0xFF})
-		} else {
-			nimg.Set(chunk.x, chunk.y, color.RGBA{0xFF, 0xFF, 0xFF, 0xFF})
+	var max, maxx, maxy int
+	var min, minx, miny int
+
+	// preprocess
+	for y := 0; y < ymax; y++ {
+
+		for x := 1; x < xmax; x++ { // x: 1 ~ xmax
+
+			r0, g0, b0, _ := img.At(x-1, y).RGBA() // returns uint32
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+
+			row_array[x-1] = int(luminosity(uint8(r0), uint8(g0), uint8(b0)) - luminosity(uint8(r1), uint8(g1), uint8(b1)))
+
+			if max < int(row_array[x-1]) {
+				max = int(row_array[x-1])
+				maxx, maxy = x, y
+			}
+			if min > int(row_array[x-1]) {
+				min = int(row_array[x-1])
+				minx, miny = x, y
+			}
+
+			r1, g1, b1, a1 := img.At(x, y).RGBA()
+			nimg.Set(x, y, color.RGBA{uint8(r1), uint8(g1), uint8(b1), uint8(a1)})
 		}
+		nimg.Set(maxx, maxy, color.RGBA{0xFF, 0x0, 0xFF, 0xFF})
+		nimg.Set(minx, miny, color.RGBA{0xFF, 0x0, 0x0, 0xFF})
+		max = 0
+		min = 255
 	}
+
 	png.Encode(os.Stdout, nimg)
 
 }
