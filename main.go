bab1638c3208dcaa47c1580cd5fed2beb165e1d7
eifu <etomita18@gmail.com>
2016-08-06 07:40:13 -0400

    add log printer

--- main.go
+++ main.go
@@ -116,7 +116,8 @@ func hough(w []image.Point, pimg image.Image) *image.RGBA {
 		maxl[r] = tmp
 		cntl[r] = cnt
 	}
-
+	log.Println("maxl: ", maxl)
+	log.Println("cntl: ", cntl)
 	// second derivative of radious candidates
 	// i, i+1, i+2, i+3, i+4
 	// \+/  \+/  \-/  \-/
@@ -135,6 +136,7 @@ func hough(w []image.Point, pimg image.Image) *image.RGBA {
 			// TODO: i or i+1 is arbitrary
 		}
 	}
+	log.Println("cc: ", cc)
 	// TODO: best 2 is arbitrary
 	// accm0, accm1: best 2 accumulation maximums
 	// cd0, cd1: best 2 candidates of radious
