71a07bffdf73643861bd2b5d1969cb0a05d8ded7
eifu <etomita18@gmail.com>
2016-08-29 22:02:54 -0400

    use filepath.Walk to find jpg files automatically

--- main.go
+++ main.go
@@ -2,32 +2,45 @@ package main
 
 import (
 	"./manaco"
+	"flag"
 	"image"
 	"image/png"
 	"log"
 	"os"
-	"strconv"
-	"time"
+	"path/filepath"
 )
 
 func main() {
-	for i := 1; i < 10; i++ {
-		submain(i)
+	flag.Parse()
+	root := flag.Arg(0)
+	err := filepath.Walk(root,
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			rel, err := filepath.Rel(root, path)
+			if err != nil {
+				return err
+			}
+			return submain(rel)
+		})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func submain(filename int) {
-	start := time.Now()
-	infile, err := os.Open("data/images/" + "AVI__Eye_378_1_4002900439_024.avi__0000" + strconv.Itoa(filename) + ".jpg")
+func submain(filepath string) error {
+	log.Println(filepath + "is loading...")
+	infile, err := os.Open("data/images/" + filepath)
 	defer infile.Close()
 	if err != nil {
 		log.Printf("main open file :%v\n", err)
-		os.Exit(1)
+		return err
 	}
 	img, _, err := image.Decode(infile)
 	if err != nil {
 		log.Printf("main read file :%v\n", err)
-		os.Exit(1)
+		return err
 	}
 
 	nimg := manaco.GaussianFilter(img)
@@ -40,15 +53,15 @@ func submain(filename int) {
 
 	nimg = manaco.Hough(w, img)
 
-	outfile, err := os.Create("result/" + strconv.Itoa(filename) + ".jpg")
+	outfile, err := os.Create("result/" + "test__" + filepath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer outfile.Close()
 
 	if err := png.Encode(outfile, nimg); err != nil {
 		log.Printf("main write file :%v\n", err)
-		os.Exit(1)
+		return err
 	}
-	log.Printf("Process took %.3fs total\n", time.Since(start).Seconds())
+	return nil
 }
