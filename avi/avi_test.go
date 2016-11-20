c71e3151cad29ef8d3c16d58330a81225263b4b7
eifu <etomita18@gmail.com>
2016-11-20 16:22:10 -0500

    add comments on avi_test.go

--- avi/avi_test.go
+++ avi/avi_test.go
@@ -7,17 +7,25 @@ import (
 )
 
 func TestRealAVIFile(t *testing.T) {
-	file, err := os.Open("test1.avi") // For read access.
+	// Read avi file
+	file, err := os.Open("test1.avi")
 	if err != nil {
 		t.Error(err)
 	}
 
+	// Construct avi struct, and read avi header.
 	avi, err := HeadReader(file)
-
 	if err != nil {
 		t.Errorf(" %#v\n", err)
 	}
+
+	// Print out what it has in the avi struct.
 	fmt.Printf("%#v \n", avi)
+
+	// Read MOVI list in the avi file.
+	// the argument is the num of frames this function reads
 	avi.MOVIReader(40)
+
+	// this prints out the entire structure of avi struct
 	avi.AVIPrint()
 }
